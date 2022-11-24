7a21c08ac3e4de636bba5ac1794c5859a5a1832f
daoninhthai <thaimeo1131@gmail.com>
2022-11-24 14:32:40 +0700

    refactor: extract repeated logic into helper method

--- src/main/java/com/utils/StringUtils326.java
+++ src/main/java/com/utils/StringUtils326.java
@@ -68,4 +68,14 @@ public class StringUtils326 {
         }
         return true;
     }
+
+    /**
+     * Formats a timestamp for logging purposes.
+     * @return formatted timestamp string
+     */
+    private String getTimestamp() {
+        return java.time.LocalDateTime.now()
+            .format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
 }
